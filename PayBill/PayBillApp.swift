239027aac958291ccf6b9812020f55630a1913eb
Sagun Raj Lage <sagoonraj@gmail.com>
2021-10-04 00:17:10 +0545

    [add] adds initial project

---
+++ PayBill/PayBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayBillApp.swift
+//  PayBill
+//
+//  Created by Sagun Raj Lage on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PayBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
